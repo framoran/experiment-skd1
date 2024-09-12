52939214406e879a3940ddc40bc9f0a481d3d6e7
Silvio Laloli <Silvio.Laloli@unige.ch>
2024-09-12 14:00:01 +0200

    instructions changes

--- resources/views/fr/instruction2.blade.php
+++ resources/views/fr/instruction2.blade.php
@@ -6,25 +6,66 @@
             @slot('title')
                 {{ config('app.name') }}
             @endslot
+            <style>
+            .container {
+              display: flex;
+              gap: 20px; /* Adds space between the boxes */
+            }
+
+            .box1, .box2 {
+              padding: 20px;
+              border: 1px solid black;
+              border-radius: 15px; /* Rounds the corners */
+            }
+
+            .box1 {
+              background-color: #0080FF; /* Color for the first box */
+            }
+
+            .box2 {
+              background-color: orange; /* Color for the second box */
+            }
+          </style>
 
             <div class="content elements-centered">
 
               <h1>
-              	Consigne
+              	Consignes
               </h1>
 
               <div>
-                Dans les pages suivantes, vous allez pouvoir vous familiariser avec le jeu.  <br> </br>
+              
+              Pour commencer, veuillez entrer votre code à 7 chiffres. <br><br>
 
-                Vous allez commencer par vous habituer au déplacement du vaisseau en utilisant les flèches haut, bas, droite et gauche.  <br> </br>
+              <form class="box">
+                <div class="field">
+                  <label class="label">Code abeille <b>bleue</b> :</label>
+                  <div class="control">
+                    <input class="input" type="text" placeholder="e.g. XFAHTIA" />
+                  </div>
+                </div>
 
-                Vous allez également vous entrainer à tirer en appuyant sur la touche "F".  <br> </br>
-
-                Appuyez sur le bouton « commencer » pour vous familiariser avec les commandes du vaisseau.  <br> </br>
-
+                <form class="box">
+                <div class="field">
+                  <label class="label">Code abeille <b>orange</b>:</label>
+                  <div class="control">
+                    <input class="input" type="text" placeholder="e.g. XFAHTIA" />
+                  </div>
+                </div>
+              </form>
               </div>
-              <a href="entrainement1" class="button is-primary mt-5"> Commencer </a>
+            <div class="container">
+            <div class="box1"> <b>Abeille bleue : pour passer à la page suivante, appuyez sur le bouton bleu.</b></div>
+            <div class="box2"> <b>Abeille orange : pour passer à la page suivante, appuyez sur le bouton orange.</b></div>
+            </div>
       </div>
+      
+      <script>
+          const url = 'http://127.0.0.1:8000/fr/instruction3';
+      </script>
+
+      <script src="/js/keydown_handler.js">
+      <script/>
     @endcomponent
   @endcomponent
 @endsection
