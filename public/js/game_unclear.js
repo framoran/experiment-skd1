3188e768d2190caef34f19a12516b3467d7a6414
David Framorando <David.Framorando@unige.ch>
2024-10-08 15:58:20 +0200

    Solved bug with multiplayer

--- public/js/game_unclear.js
+++ public/js/game_unclear.js
@@ -729,6 +729,8 @@ function updateGame() {
     if (!multiplayer && controle1){
         myGamePiece_Fleurs2.draw();
         updateVaisseau2(); // Update the new Vaisseau
+    }else if (multiplayer){
+        updateVaisseau2(); // Update the new Vaisseau
     }
     myGamePiece_Evenements.update();    
     this.score = myGamePiece_Affichage.score1;
@@ -736,5 +738,7 @@ function updateGame() {
     context.fillStyle = "#FFFFFF";
     if (!multiplayer && !controle1){
         myGamePiece_Vaisseau.update()
-    }
+    }else if (multiplayer){
+        myGamePiece_Vaisseau.update()
+    }   
 };
